59e1c170f96dd320e2d11b439f3f4011778b0dd7
oj4wipro <12714819+oj4wipro@users.noreply.github.com>
2025-06-05 17:01:46 +0200

    Beispiel "example_Web_Mercator_Auxiliary_Sphere_3857.js" um Zoom- und Mausposition-Komponente erweitert

--- examples/example_Web_Mercator_Auxiliary_Sphere_3857.js
+++ examples/example_Web_Mercator_Auxiliary_Sphere_3857.js
@@ -1,15 +1,29 @@
 import geolineOlJs from "../src/geoline.ol";
 //import geolineOlJs from "@stadtmessungsamt-stuttgart/geoline.ol.js";
 import "../src/geoline.ol.css";
+import MousePosition from 'ol/control/MousePosition.js';
+import Zoom from 'ol/control/Zoom.js';
 
 var mymap;
 $(function() {
 	mymap = new geolineOlJs();
 	mymap.initMap(3857, {}, {
-		center: [1031492, 6220413],
-		zoom: 10
+		center: [1021708, 6237140],
+		zoom: 12
 	});
-	
+
 	mymap.addStmaBaseLayer("Grundkarte");
 	mymap.addEsriLayer("https://server.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer");
+
+	const mousePosition = new MousePosition({
+		coordinateFormat: function(coordinate) {
+			console.error(coordinate);
+			return (Math.round(coordinate[0] * 1000) / 1000).toString() + " / " +
+				(Math.round(coordinate[1] * 1000) / 1000).toString()
+		}
+	});
+	mymap.getMap().addControl(mousePosition);
+
+	const zoom = new Zoom({});
+	mymap.getMap().addControl(zoom);
 });
