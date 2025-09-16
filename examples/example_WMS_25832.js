00a11082b91a77202ed4b588479498fc1eb364e5
oj4wipro <12714819+oj4wipro@users.noreply.github.com>
2025-09-16 07:52:27 +0200

    GEOJS-9 Zweiter Versuch für "example_WMS_25832.js"

--- examples/example_WMS_25832.js
+++ examples/example_WMS_25832.js
@@ -7,7 +7,10 @@ const mymap = new StmaOpenLayers();
 mymap.initMap(
     25832,
     {target: "the-map"},
-    {zoom: 6},
+    {
+        center: [513422, 5403039], // somewhere inside Stuttgart in EPSG:25832
+        zoom: 14
+    },
     {},
     function (map) {
         console.log("initMap wurde ausgeführt");
@@ -17,6 +20,46 @@ mymap.initMap(
 mymap.addStmaWMSLayer(
     "Base:A62_Luftbild_2009_EPSG25832",
     {},
+    {
+        TILED: true,   // make sure we request cached tiles
+        serverType: "geoserver",
+        crossOrigin: "anonymous",
+        params: {
+            VERSION: "1.3.0",
+            FORMAT: "image/png",
+            TRANSPARENT: true,
+            STYLES: ""
+        }
+    },
+    (layer) => console.log("STMA WMS OK", layer)
+);
+/**/
+
+const mymap = new StmaOpenLayers();
+mymap.initMap(
+    25832,
+    {
+        target: "the-map"
+    },
+    {
+        center: [513422, 5403039], // somewhere inside Stuttgart in EPSG:25832
+        zoom: 14
+    }
+);
+
+mymap.addStmaWMSLayer(
+    "Base:A62_Luftbild_2009_EPSG25832",
     {},
+    {
+        TILED: true,   // make sure we request cached tiles
+        serverType: "geoserver",
+        crossOrigin: "anonymous",
+        params: {
+            VERSION: "1.3.0",
+            FORMAT: "image/png",
+            TRANSPARENT: true,
+            STYLES: ""
+        }
+    },
     (layer) => console.log("STMA WMS OK", layer)
 );
